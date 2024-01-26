8319b10386b24b69e6c60c84d27e5500a2d598de
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-01-26 00:05:57 -0500

    Update WordCompare.py

--- WordCompare.py
+++ WordCompare.py
@@ -1,1 +1,8 @@
+def word_compare(str1, str2): 
+ if not isinstance(str1, str) or not
+sinstance(str2, str): 
+  return ""Those aren't strings" 
 
+if sorted(str1) == sorted(str2): 
+  return "Anagram" 
+  return (str1, str2)
