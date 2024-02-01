b6bdd44decfe033c7ad41513ba0bd2ad03fa0b74
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-02-01 17:54:10 -0500

    Update

---
+++ TestHw2.py
@@ -0,0 +1,9 @@
+class TestCard(unittest.TestCase):
+def test_init():
+"""Tests that we can initialize cards w/ number/color/shading/shape"""
+
+def test_str():
+"""test that we can get a good string representation of Card instances"""
+
+def test_eq(self):
+"""Tests that two cards are equal iff all attributes are equal"""
