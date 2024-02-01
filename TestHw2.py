bd37ca37ed190cea82c4bb0845c63c825eca4684
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-02-01 18:09:00 -0500

    Update TestHw2.py

--- TestHw2.py
+++ TestHw2.py
@@ -1,12 +1,22 @@
 class TestCard(unittest.TestCase):
 
 def test_init():
-c1 = Card(2, "green", "striped", "diamond")
+c1 = Card(1, "green", "empty", "diamond")
 >>> c1.number
+1
+
+c2= Card(3, "oval", "purple", "solid")
+>>> c2.number 
+3
+
+c3= Card(2, "squiggle", "blue", "striped")
+>>> c3.number 
 2
+
 """Tests that we can initialize cards w/ number/color/shading/shape"""
 
 def test_str():
+
 """test that we can get a good string representation of Card instances"""
 
 def test_eq(self):
