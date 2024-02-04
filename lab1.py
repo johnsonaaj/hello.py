84d81282ef17397184c94e2097a988c62316af2f
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-02-03 20:36:59 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,6 +1,3 @@
-def generic_hi(name):
- user_input = ('') 
- if len(user_input) == 0 :
-   user_input = "world!"
- greet = "Hello, " + user_input
- return greet 
+def generic_hi(name): 
+    greeting = "Hello, " + name + "!" 
+    return greeting  
