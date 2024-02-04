fcd85d56147a294c8a1100e43d1c277d4ebbf8a2
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-02-03 20:16:36 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
-def generic_hi(name):
+def generic_hi():
  user_input = ("") 
-  if len(user_input) == 0 :
-    user_input = "World"
+ if len(user_input) == 0 :
+   user_input = "World"
  return "Hello,", user_input 
