8d53ab4f6ca32eff3c139cf332efbdec32571d3b
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-02-03 22:28:27 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,8 +1,8 @@
 
-def generic_hi (self,name): 
-  pass
+def generic_hi (name): 
   greeting = "Hello, " + name + "!"
   if len(name) == 0 :
    name = "world!"
   return greeting  
- 
+
+print(generic_hi())
