97b64bbb20dc42d0b8d3c8bfc2cee498926e8087
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-04-25 10:11:37 -0400

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -11,35 +11,35 @@ class Entry:
 
 class PQ_UL:
   def __init__(self):
-      self.1 =[]
+      self.l =[]
   def __len__(self):
-      return len(self.1)
+      return len(self.l)
   def insert(self, item, priority):
-      self.1.append(Entry(item, priority))
+      self.l.append(Entry(item, priority))
   def find_min(self):
-      entry = self.1[0]
-      for i in range(len(self.1)-1):
-          if self.1[i + 1].priority < entry.priority:
-                entry=self.1[i+1]
+      entry = self.l[0]
+      for i in range(len(self.l)-1):
+          if self.l[i + 1].priority < entry.priority:
+                entry=self.l[i+1]
       return entry 
   def remove_min(self):
-      entry = self.1[0]
+      entry = self.l[0]
       index = 0
-      for i in range(len(self.1)-1):
-          if self.1[i+1].priority < entry. priority:
-              entry = self.1[i +1]
+      for i in range(len(self.l)-1):
+          if self.l[i+1].priority < entry. priority:
+              entry = self.l[i +1]
               index = i + 1 
-      self.1.pop(index)
+      self.l.pop(index)
       return entry 
 class PQ_OL:
   def __init__(self):
-      self.1[]
+      self.l=[]
   def __len__(self):
-      return len(self.1)
+      return len(self.l)
   def insert(self, item, priority):
-      self.1.append(Entry(item, priority))
-      self.1.sort(reverse = True)
+      self.l.append(Entry(item, priority))
+      self.l.sort(reverse = True)
   def find_min(self):
-      return self.1[-1]
+      return self.l[-1]
   def remove_min(self):
-      return self.1.pop(-1)
+      return self.l.pop(-1)
