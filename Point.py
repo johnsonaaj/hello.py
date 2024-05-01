f417b685cd25b36fe01ff10017658eddb7272a5a
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2024-05-01 18:54:34 -0400

    Update Point.py

--- Point.py
+++ Point.py
@@ -5,15 +5,16 @@ class Point:
        def __eq__(self, other):
               if self.x == other.x and self.y== other.y:
                     return True
-              else: 
+              else:
                     return False
        def equidistant(self, other):
               if ((self.x**2 + self.y**2)**1/2) == ((other.x**2+ other.y**2)**1/2):
                     return True
               else:
                      return False
-        def within(self. distance, other):
+       def within(self, distance, other):
               if ((((other.x - self.x)**2) + ((other.y - self.y)**2))**(1/2)) <= distance:
                     return True
               else:
                     return False 
+ 
