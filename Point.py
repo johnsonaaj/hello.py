ddea55c8ecd31e9b559e2f9a61ed838f279898f5
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2025-02-13 06:56:40 -0500

    Point.py

--- Point.py
+++ Point.py
@@ -1,20 +1,20 @@
 class Point:
-       def __init__(self, x, y):
-             self.x = x
-             self.y = y
-       def __eq__(self, other):
-              if self.x == other.x and self.y== other.y:
-                    return True
-              else:
-                    return False
-       def equidistant(self, other):
-              if ((self.x**2 + self.y**2)**1/2) == ((other.x**2+ other.y**2)**1/2):
-                    return True
-              else:
-                     return False
-       def within(self, distance, other):
-              if ((((other.x - self.x)**2) + ((other.y - self.y)**2))**(1/2)) <= distance:
-                    return True
-              else:
-                    return False 
- 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+""" Sees if two points are equal to eachother"""
+    def __lt__(self, other):
+        return self.dist_from_origin() < other.dist_from_origin()
+"""Sees which point has a lesser distance from orgin"""
+    def __gt__(self, other):
+        return self.dist_from_origin() > other.dist_from_origin()
+"""See which point has a greater distance from orgin"""
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+"""Formats point into a string"""
+    def dist_from_origin(self):
+        return (self.x**2 + self.y**2) ** 0.5
+"""Finds distance from orgin"""
