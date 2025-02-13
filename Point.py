27f5c6d7547001345e8a34cac9c932a1492d6d73
johnsonaaj <157259173+johnsonaaj@users.noreply.github.com>
2025-02-13 09:39:52 -0500

    Point.py

--- Point.py
+++ Point.py
@@ -1,20 +1,25 @@
 class Point:
     def __init__(self, x, y):
+        """Initializes a point with x and y coordinates."""
         self.x = x
         self.y = y
 
     def __eq__(self, other):
+        """Sees if two points are equal to each other."""
         return self.x == other.x and self.y == other.y
-""" Sees if two points are equal to eachother"""
+
     def __lt__(self, other):
+        """Sees which point has a lesser distance from the origin."""
         return self.dist_from_origin() < other.dist_from_origin()
-"""Sees which point has a lesser distance from orgin"""
+
     def __gt__(self, other):
+        """Sees which point has a greater distance from the origin."""
         return self.dist_from_origin() > other.dist_from_origin()
-"""See which point has a greater distance from orgin"""
+
     def __str__(self):
+        """Formats point into a string."""
         return f"({self.x}, {self.y})"
-"""Formats point into a string"""
+
     def dist_from_origin(self):
+        """Finds distance from the origin."""
         return (self.x**2 + self.y**2) ** 0.5
-"""Finds distance from orgin"""
